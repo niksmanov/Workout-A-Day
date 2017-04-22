83e41064e4a1bbc2254c41cc780e503d1c07d1c2
vasilvalkov <vasil.valkov@gmail.com>
2017-04-22 20:29:41 +0300

    Added event listener to register button to load page

--- app-modules/index.js
+++ app-modules/index.js
@@ -1,7 +1,12 @@
-const ref = firebase.database().ref();
+import { templates } from 'templates';
+import 'jquery';
 
-ref.on("value", function(snapshot) {
-    console.log(snapshot.val());
-}, function (error) {
-    console.log("Error: " + error.code);
-});
+$('#registerBtn').on('click', () => templates.getPage('register', {}));
+
+// const ref = firebase.database().ref();
+
+// ref.on("value", function(snapshot) {
+//     console.log(snapshot.val());
+// }, function (error) {
+//     console.log("Error: " + error.code);
+// });
