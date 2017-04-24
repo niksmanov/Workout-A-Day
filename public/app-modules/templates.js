8fd3ab2a7ad2ac22f8f9f527b3845c7ed689862d
vasilvalkov <vasil.valkov@gmail.com>
2017-04-24 14:33:49 +0300

    getPage now returns the promise from the get request in order to enable loading event for the newly loaded page elements

--- public/app-modules/templates.js
+++ public/app-modules/templates.js
@@ -3,7 +3,7 @@ import 'jquery';
 const templates = (function () {
     function getPage(pageName, data) {
         const url = `templates/${pageName}.handlebars`;
-        $.get(url, function (html) {
+        return $.get(url, function (html) {
             const hbTemplate = Handlebars.compile(html.toString());
             $('#content').html(hbTemplate(data));
         });
