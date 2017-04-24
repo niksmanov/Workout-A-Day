23bd8ac08c4d374200ed4b782e29ba0a92c201b0
vasilvalkov <vasil.valkov@gmail.com>
2017-04-24 15:14:32 +0300

    Created loginController.js to handle user login

---
+++ public/controllers/loginController.js
@@ -0,0 +1,33 @@
+import { templates } from 'templates';
+
+const loginController = function (user) {
+    templates.getPage('login', {})
+        .done(() => {
+
+        const $loginBtn = $('#btn-login');
+        const $email = $('#tb-email');
+        const $pass = $('#tb-password');
+
+        $loginBtn.on('click', () => {
+            if($email.val() && $pass.val()){
+
+                const user = firebase.auth().signInWithEmailAndPassword($email.val(), $pass.val())
+                    .catch(function(error) {
+                        const errorCode = error.code;
+                        const errorMessage = error.message;
+                        toastr.error(`There was an error: ${errorCode} - ${errorMessage}
+                        Please try again.`);
+                    });
+
+                user.then((usr) => {
+                    toastr.success(`You have successfully logged in as ${usr.displayName}`);
+                    location.hash = '/home';
+                });
+            } else {
+                toastr.error('fill in all fields');
+            }  
+        });
+    });
+}
+
+export { loginController };
