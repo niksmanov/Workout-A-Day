a63c40cac85106d748dc50a9635a52fdcaa862c5
vasilvalkov <vasil.valkov@gmail.com>
2017-04-24 14:39:10 +0300

    Added user registration functionality

---
+++ public/controllers/registerController.js
@@ -0,0 +1,34 @@
+import { templates } from 'templates';
+
+const registerController = function () {
+    templates.getPage('register', {})
+        .done(() => {
+
+        const $registerBtn = $('#btn-register');
+        const $username = $('#tb-username');
+        const $email = $('#tb-email');
+        const $pass = $('#tb-password');
+
+        $registerBtn.on('click', () => {
+            if($email.val() && $pass.val()){
+
+                const user = firebase.auth().createUserWithEmailAndPassword($email.val(), $pass.val())
+                    .catch(function(error) {
+                        const errorCode = error.code;
+                        const errorMessage = error.message;
+                        toastr.error(`There was an error: ${errorCode} - ${errorMessage}
+                        Please try again.`);
+                    });
+
+                user.then((usr) => {
+                    toastr.success(`You have successfully registered as ${$username.val()}`);
+                    location.hash = '/home';
+                });
+            } else {
+                toastr.error('fill in all fields');
+            }  
+        });
+    });
+}
+
+export { registerController };
