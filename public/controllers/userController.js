216d3760ee6f6a6c68f50a0b981901c43d13f693
vasilvalkov <vasil.valkov@gmail.com>
2017-04-25 12:18:58 +0300

    Implement userController to load user page with details

---
+++ public/controllers/userController.js
@@ -0,0 +1,37 @@
+import { templates } from 'templates';
+
+const userController = function (user) {
+    templates.getPage('user', user)
+        .done((user) => {
+
+        // const $loginBtn = $('#btn-login');
+        // const $email = $('#tb-email');
+        // const $pass = $('#tb-password');
+
+        // $loginBtn.on('click', () => {
+        //     if($email.val() && $pass.val()){
+
+        //         const user = firebase.auth().signInWithEmailAndPassword($email.val(), $pass.val())
+        //             .catch(function(error) {
+        //                 const errorCode = error.code;
+        //                 const errorMessage = error.message;
+        //                 toastr.error(`There was an error: ${errorCode} - ${errorMessage}
+        //                 Please try again.`);
+        //             });
+
+        //         user.then((usr) => {
+        //             $('#loginBtn').addClass('hidden');
+        //             $('#registerBtn').addClass('hidden');
+        //             $('#currentUser').removeClass('hidden').text(`Hello, ${usr.displayName}`);
+        //             $('#logoutBtn').removeClass('hidden');
+        //             toastr.success(`You have successfully logged in as ${usr.displayName}`);
+        //             location.hash = '/home';
+        //         });
+        //     } else {
+        //         toastr.error('fill in all fields');
+        //     }  
+        // });
+    });
+};
+
+export { userController };
