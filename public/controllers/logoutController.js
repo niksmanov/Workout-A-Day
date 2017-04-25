3eed3c74962cfaa50cc385c2d2d160181a31bc41
vasilvalkov <vasil.valkov@gmail.com>
2017-04-25 10:34:12 +0300

    Implement logout controller

---
+++ public/controllers/logoutController.js
@@ -0,0 +1,22 @@
+const logoutController = function (user) {
+    if (user) {
+        firebase.auth().signOut()
+            .then(function () {
+                $('#currentUser').addClass('hidden').text(``);
+                $('#logoutBtn').addClass('hidden');
+                $('#loginBtn').removeClass('hidden');
+                $('#registerBtn').removeClass('hidden');
+
+                toastr.success(`You have successfully logged out!`);
+                location.hash = '/home';
+            })
+            .catch(function (error) {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                toastr.error(`There was an error: ${errorCode} - ${errorMessage}
+                        Please try again.`);
+            });
+    };
+};
+
+export { logoutController };
