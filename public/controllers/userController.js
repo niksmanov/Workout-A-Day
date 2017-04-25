495834ce370a03737d4cd00a6ea8e1cc8f379be4
vasilvalkov <vasil.valkov@gmail.com>
2017-04-25 15:47:19 +0300

    Removed redundant calls

--- public/controllers/userController.js
+++ public/controllers/userController.js
@@ -4,13 +4,7 @@ import { changePasswordController } from 'changePasswordController';
 
 const userController = function (user) {
     templates.getPage('user', user)
-        .done(() => {
-            const $editProfileBtn = $('#editProfileBtn');
-            const $changePasswordBtn = $('#changePasswordBtn');
-
-            $editProfileBtn.on('click', () => editProfileController(user));
-            $changePasswordBtn.on('click', () => changePasswordController(user));
-    });
+        .done();
 };
 
 export { userController };
