f069745ff823e884009e81767aa4c238bd365908
vasilvalkov <vasil.valkov@gmail.com>
2017-04-26 12:44:11 +0300

    Implemented change password functionality

--- public/controllers/changePasswordController.js
+++ public/controllers/changePasswordController.js
@@ -4,25 +4,41 @@ const changePasswordController = function (user) {
     templates.getPage('changePassword', user)
         .done(() => {
             const $changeBtn = $('#changeBtn');
+            const $currentPass = $('#tb-currentPass');
             const $newPass = $('#tb-newPassword');
             const $confirmPass = $('#tb-confirmPassword');
 
             $changeBtn.on('click', () => {
-                const confirmPass = $currentPass.val();
                 const newPass = $newPass.val();
+                const confirmPass = $confirmPass.val();
 
                 if (confirmPass !== newPass) {
                     toastr.error('Passwords do not match!');
                 } else if (newPass.trim() === '') {
                     toastr.error('The new password cannot be empty!');
                 } else {
-                    const updated = firebase.User.updatePassword(newPass);
+                    const currentUser = firebase.auth().currentUser;
+                    const credential = firebase.auth.EmailAuthProvider.credential(user.email, $currentPass.val());
 
-                    updated
-                        .then(() => {
-                            toastr.success('You have changed your password successfully!');
-                            location.hash('/user');
-                        });
+                    const reauth = Promise.resolve(currentUser.reauthenticateWithCredential(credential));
+
+                    reauth
+                        .then(() => {                            
+                            const updated = Promise.resolve(currentUser.updatePassword(newPass));
+                            
+                            updated
+                                .then(() => {
+                                    toastr.success('You have changed your password successfully!');
+                                    location.hash = '/user';
+                                },
+                                function(error) {
+                                    toastr.error(`${error.message}`);
+                            });
+                        },
+                        function (error) {
+                            toastr.error(`${error.message}`);
+                        }
+                    );
                 }
 
             });
