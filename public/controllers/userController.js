95841b7b2a0927543129d1a0dec08d9e111e5da0
vasilvalkov <vasil.valkov@gmail.com>
2017-04-26 13:39:43 +0300

    Removes redundant imports

--- public/controllers/userController.js
+++ public/controllers/userController.js
@@ -1,6 +1,4 @@
 import { templates } from 'templates';
-import { editProfileController } from 'editProfileController';
-import { changePasswordController } from 'changePasswordController';
 
 const userController = function (user) {
     templates.getPage('user', user)
