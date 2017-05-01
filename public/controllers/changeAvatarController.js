f83c389d31a4cba7c96c141fc75f9bf0fec89599
vasilvalkov <vasil.valkov@gmail.com>
2017-05-01 18:00:01 +0300

    Implement uploading new avatar and attaching to profile

--- public/controllers/changeAvatarController.js
+++ public/controllers/changeAvatarController.js
@@ -1,4 +1,4 @@
-import { templates } from 'templates';
+import { templates} from 'templates';
 
 const changeAvatarController = function (user) {
     if (user) {
@@ -6,12 +6,26 @@ const changeAvatarController = function (user) {
             .done(() => {
                 const input = document.getElementById('fileInput');
                 const uploadBtn = document.getElementById('uploadBtn');
-                // const $input = $('#fileInput');
-                // const $uploadBtn = $('#uploadBtn');
+                const extension = input.value.split('.').pop();
+                const fileName = `${user.displayName}_${Date.now()}.${extension}`;
+                const storageRef = firebase.storage().ref(`avatars/${fileName}`);
+
+                storageRef.put(input.files[0])
+                    .then(snapshot => {
+                        user.updateProfile({
+                                photoURL: storageRef.fullPath
+                            })
+                            .then(() => {
+                                toastr.success('Your avatar was changed successfully!');
+                                location.hash = '/user';
+                            });
+                    });
             });
     } else {
         location.hash = '/login';
     }
 };
 
-export { changeAvatarController };
+export {
+    changeAvatarController
+};
