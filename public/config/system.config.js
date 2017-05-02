746b7db199eeac1af58df1697401cfd751f0c602
niksmanov <niks.manov@gmail.com>
2017-05-02 21:28:38 +0300

    restore old system.config

--- public/config/system.config.js
+++ public/config/system.config.js
@@ -1,9 +1,5 @@
 SystemJS.config({
-    transpiler: "plugin-babel",
-    babelOptions: {
-        plugins: ["transform-es2015-modules-systemjs"],
-        presets: ['es2015', 'react']
-    },
+    "transpiler": "plugin-babel",
     "map": {
         //SystemJS files\\
         "plugin-babel": "../node-modules/systemjs-plugin-babel/plugin-babel.js",
@@ -30,13 +26,13 @@ SystemJS.config({
 
 
         //Libraries\\
-        "jquery": "../node-modules/jquery/dist/jquery.js",
+        "jquery" : "../node-modules/jquery/dist/jquery.js",
         //Routing:
         "navigo": "../node-modules/navigo/lib/navigo.min.js",
         //HTML:
         "handlebars": "../node-modules/handlebars/dist/handlebars.js",
         //Popup messages
-        "toastr": "../node-modules/toastr/toastr.js",
+        "toastr" : "../node-modules/toastr/toastr.js",
         //UI stuffs (autocomplete, calendar etc.)
         "jqueryUi": "../node-modules/jquery-ui-dist/jquery-ui.js",
         "bootstrap": "node-modules/bootstrap/dist/js/bootstrap.min.js"
