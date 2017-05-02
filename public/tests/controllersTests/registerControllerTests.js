8ef99eb7f5111bf3f2a9cbf093d09ad4981f0a80
niksmanov <niks.manov@gmail.com>
2017-05-03 00:24:23 +0300

    Fix test running

--- public/tests/controllersTests/registerControllerTests.js
+++ public/tests/controllersTests/registerControllerTests.js
@@ -1,5 +1,13 @@
+import {templates} from 'templates';
+import {UserRequester} from 'userRequester';
+
+mocha.setup('bdd');
+const expect = chai.expect;
+
 describe('Register controller tests', () => {
     it('It works', () => {
         expect(2 + 2).to.equal(4);
     });
 });
+
+mocha.run();
