d8ca182cc930841eee032d285536dbb08a0e5d44
vasilvalkov <vasil.valkov@gmail.com>
2017-05-02 11:29:26 +0300

    Added input file validations

--- public/controllers/changeAvatarController.js
+++ public/controllers/changeAvatarController.js
@@ -10,6 +10,19 @@ const changeAvatarController = function (user) {
                     const input = document.getElementById('fileInput');
                     const newAvatar = input.files[0];
                     const extension = input.value.split('.').pop();
+
+                    const acceptedExtensions = ['.jpg', '.jpeg', '.png'];
+                    if (acceptedExtensions.indexOf(extension.toLowerCase() < 0)) {
+                        toastr.error('The accespted file formats are .jpeg and .png!');
+                        return;
+                    }
+
+                    const bytesInOneMegabyte = 1048576;
+                    if (newAvatar.size > bytesInOneMegabyte) {
+                        toastr.error('The file cannot be more than 1MB in size!');
+                        return;
+                    }
+
                     const userName = user.displayName.replace(' ', '_');
                     const newAvatarFileName = `${userName}_${Date.now()}.${extension}`;
                     const storageRef = firebase.storage().ref(`avatars/${newAvatarFileName}`);
