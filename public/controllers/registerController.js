ffe1c6aa3ab7da9abc7373b211ec14272f691d21
niksmanov <niks.manov@gmail.com>
2017-05-02 21:26:11 +0300

    Import testing controllers functionality

--- public/controllers/registerController.js
+++ public/controllers/registerController.js
@@ -1,43 +1,43 @@
-import { templates } from 'templates';
-import { UserRequester } from 'userRequester';
+import {templates} from 'templates';
+import {UserRequester} from 'userRequester';
 
 const registerController = function () {
     templates.getPage('register', {})
         .done(() => {
 
-        const $registerBtn = $('#btn-register');
-        const $username = $('#tb-username');
-        const $email = $('#tb-email');
-        const $pass = $('#tb-password');
+            const $registerBtn = $('#btn-register');
+            const $username = $('#tb-username');
+            const $email = $('#tb-email');
+            const $pass = $('#tb-password');
 
-        $registerBtn.on('click', () => {
-            if($email.val() && $pass.val()){
-                const userRequester = new UserRequester();
+            $registerBtn.on('click', () => {
+                if ($email.val() && $pass.val()) {
+                    const userRequester = new UserRequester();
 
-                const user = userRequester.createUserWithEmailAndPassword($email.val(), $pass.val())
-                    .catch(function(error) {
-                        const errorCode = error.code;
-                        const errorMessage = error.message;
-                        toastr.error(`There was an error: ${errorCode} - ${errorMessage}
+                    const user = userRequester.createUserWithEmailAndPassword($email.val(), $pass.val())
+                        .catch(function (error) {
+                            const errorCode = error.code;
+                            const errorMessage = error.message;
+                            toastr.error(`There was an error: ${errorCode} - ${errorMessage}
                         Please try again.`);
-                    });
-
-                user.then((usr) => {
-                    userRequester.currentUser.updateProfile({ 
-                        displayName: $username.val(),
-                        photoURL: "images/default-user.jpg"
-                    })
-                        .then(() => {
-                            toastr.success(`You have successfully registered as ${$username.val()}`);
-                            location.hash = '/home';
-                            location.reload();
                         });
-                });
-            } else {
-                toastr.error('Fill all the fields!');
-            }  
+
+                    user.then((usr) => {
+                        userRequester.currentUser.updateProfile({
+                            displayName: $username.val(),
+                            photoURL: "images/default-user.jpg"
+                        })
+                            .then(() => {
+                                toastr.success(`You have successfully registered as ${$username.val()}`);
+                                location.hash = '/home';
+                                location.reload();
+                            });
+                    });
+                } else {
+                    toastr.error('Fill all the fields!');
+                }
+            });
         });
-    });
 };
 
-export { registerController };
+export {registerController};
---
+++ public/tests/controllersTests/registerControllerTests.js
@@ -0,0 +1,5 @@
+describe('Register controller tests', () => {
+    it('It works', () => {
+        expect(2 + 2).to.equal(4);
+    });
+});
