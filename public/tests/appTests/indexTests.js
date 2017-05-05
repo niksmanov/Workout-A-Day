c1d07eb413765d2a40ff0dae1457d121813e4420
vasilvalkov <vasil.valkov@gmail.com>
2017-05-05 23:02:37 +0300

    setup test

--- public/tests/appTests/indexTests.js
+++ public/tests/appTests/indexTests.js
@@ -1,4 +1,15 @@
-
+import {templates} from 'templates';
+import {registerController} from 'registerController';
+import {loginController} from 'loginController';
+import {logoutController} from 'logoutController';
+import {userController} from 'userController';
+// import {homeController} from 'homeController';
+// import {galleryController} from 'galleryController';
+import {trainingsController} from 'trainingsController';
+import {changePasswordController} from 'changePasswordController';
+import {changeAvatarController} from 'changeAvatarController';
+import {editProfileController} from 'editProfileController';
+import {videosController} from 'videosController';
 import {UserRequester} from 'userRequester';
 
 mocha.setup('bdd');
@@ -6,7 +17,13 @@ const expect = chai.expect;
 
 describe('Index file tests', () => {
     it('It works for index', () => {
-        expect(2 + 2).to.equal(4);
+        const loginControllerMock = sinon.spy(loginController);
+
+        location.hash = '/login';
+
+        expect(loginControllerMock).to.have.been.calledOnce;
+
+        loginControllerMock.restore();
     });
 });
 
