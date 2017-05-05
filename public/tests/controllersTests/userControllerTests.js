7df26f4f15d1622d22ae5bb5912d07d470d483a4
simzzz <swagb3ast@hotmail.com>
2017-05-05 20:16:08 +0300

    Added test for redirection if there is no current user

--- public/tests/controllersTests/userControllerTests.js
+++ public/tests/controllersTests/userControllerTests.js
@@ -6,6 +6,7 @@ mocha.setup('bdd');
 const expect = chai.expect;
 
 describe('User controller tests', () => {
+
     it('Expect userController.js to exist', () => {
         expect(userController).to.exist;
     });
@@ -14,7 +15,13 @@ describe('User controller tests', () => {
         expect(userController).to.be.a('function');
     });
 
-
+    it('Expect userController to redirect to login if there is no user.', function () {
+        var user = UserRequester.currentUser;
+        if (!(user)) {
+            userController(user);
+            expect(location.hash).to.equal('#/login');
+        }
+    });
 });
 
 mocha.run();
